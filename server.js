fcd430ad571cd1050c32a7d2781c1cea11f15186
jim37500 <jim37500@gmail.com>
2025-04-19 20:46:50 +0800

    NEW - Common - 新增 Dockerfile

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 async function startServer() {
-    const app = express();
+  const app = express();
 
-    // Serve static files from the build directory
-    app.use(express.static(path.resolve(__dirname, 'dist')));
+  // Serve static files from the build directory
+  app.use(express.static(path.resolve(__dirname, 'dist')));
 
-    // For every other request, send back index.html
-    app.get('*', async (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-    });
+  // For every other request, send back index.html
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
 
-    const port = 61018;
-    app.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
-    });
+  const port = 61018;
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
 }
 
 startServer();
